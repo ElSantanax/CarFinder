74b900045fbc32fccb61adb261828783aae25d5b
ElSantanax <josesantanaserrano130598@gmail.com>
2025-06-26 08:07:01 -0500

    Agregar funcionalidad para llenar el selector de años y mejorar la estructura del código en app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+const max = new Date().getFullYear();
+const min = max - 10;
 
 document.addEventListener('DOMContentLoaded', () => {
     mostrarAutos();
+    llenarSelect();
 })
 
+// Funciones
 function mostrarAutos() {
     autos.forEach(auto => {
-
         const autoHTML = document.createElement('P');
         const { marca, modelo, year, precio, puertas, color, transmision } = auto
 
@@ -16,4 +21,13 @@ function mostrarAutos() {
 
         resultado.appendChild(autoHTML);
     });
+}
+
+function llenarSelect() {
+    for (let i = max; i >= min; i--) {
+        const opcion = document.createElement('OPTION');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion)
+    }
 }
